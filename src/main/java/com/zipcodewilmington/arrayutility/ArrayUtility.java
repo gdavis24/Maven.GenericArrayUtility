38f96e6a3b625e19010dcc415dd8702a05124159
Gregory Davis <gdavis@zipcodes-MacBook-Pro-2.local>
2020-03-09 18:35:55 -0400

    red squiggles are gone

--- src/main/java/com/zipcodewilmington/arrayutility/ArrayUtility.java
+++ src/main/java/com/zipcodewilmington/arrayutility/ArrayUtility.java
@@ -3,5 +3,32 @@ package com.zipcodewilmington.arrayutility;
 /**
  * Created by leon on 3/6/18.
  */
-public class ArrayUtility {
+public class ArrayUtility <T>{
+    private T[] genArray;
+
+
+
+    public ArrayUtility(T[] testArray){
+        this.genArray = testArray;
+    }
+
+//    public T[] getObject(){
+//        return this.genArray;
+//    }
+
+    public Integer countDuplicatesInMerge(T[] arrayToMerge, T valueToEvaluate) {
+        return null;
+    }
+
+    public T getMostCommonFromMerge(T[] arrayToMerge) {
+        return null;
+    }
+
+    public Integer getNumberOfOccurrences(T valueToEvaluate) {
+        return null;
+    }
+
+    public T[] removeValue(T valueToRemove) {
+        return null;
+    }
 }
